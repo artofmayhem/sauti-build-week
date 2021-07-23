22d0f4e09a308749777dd855c3d1268fabf09b52
artofmayhem <chef_tony_miller@yahoo.com>
2021-07-22 18:58:03 -1000

    server running migrations and seeds complete

--- api/server.js
+++ api/server.js
@@ -2,16 +2,10 @@ const express = require("express")
 const cors = require("cors")
 const helmet = require("helmet")
 
-
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
-
-
-
-
-
 
 server.get("/",(req, res) =>{
     res.json({yodaSays:"Server up it is"})
