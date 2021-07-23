fc120c563ed6d3fdf7d107a0246e1e80f70b998c
artofmayhem <chef_tony_miller@yahoo.com>
2021-07-22 18:39:09 -1000

    third set of dependencies

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+
+
+
+
+
+
+server.get("/",(req, res) =>{
+    res.json({yodaSays:"Server up it is"})
+})
+module.exports = server;
